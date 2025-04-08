fe1579a9a70863ff973c23f95b1007ea9ec4002c
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2025-04-08 17:17:26 +0200

    feat: added window

--- main.c
+++ main.c
@@ -5,13 +5,19 @@ int main(int nargs, char **args)
     t_fractal fractal;
     if ((nargs == 2 && !ft_strcmp(args[1], "mandelbrot", 10)) || (nargs == 4 && !ft_strcmp(args[1], "julia", 10)))
     {
-        fractal_init(&fractal);
-        fractal_render(&fractal);
-        mlx_loop(fractal.mlx_connection);
+        void *mlx;
+        void *mlx_win;
+
+        mlx = mlx_init();
+        mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+        mlx_loop(mlx);
+        // fractal_init(&fractal);
+        // fractal_render(&fractal);
+        // mlx_loop(fractal.mlx_connection);
     }
     else
     {
-        putstr_fd(ERROR_MESSAGE, 1);
+        // putstr_fd(ERROR_MESSAGE, 1);
         exit(EXIT_FAILURE);
     }
 }
