ea98109d4aed765124a24b42c4ba345e225c83da
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2025-04-13 16:02:45 +0200

    Update render.c

--- render.c
+++ render.c
@@ -26,7 +26,7 @@ static void handle_pixel(int x, int y, t_fractal *fractal)
         z = sum_complex(square_complex(z), c);
         if ((z.x * z.x) + (z.y * z.y) > fractal->scape_value)
         {
-            color = scale(i, GLOW_ORANGE, PSY_RED, 0, fractal->ilterations); // Fractal shape with orange tones
+            color = scale(i, HYPER_TEAL, ULTRA_VIOLET, 0, fractal->ilterations); // Fractal shape with teal and purple tones
             my_pixel_put(x, y, &fractal->img, color);
             return;
         }
@@ -47,7 +47,7 @@ void fractal_render(t_fractal *fractal)
         while (x < WIDTH)
         {
             if (x == 0 || y == 0 || x == WIDTH - 1 || y == HEIGHT - 1) // Set background color
-                my_pixel_put(x, y, &fractal->img, 0xFFAA33);           // Light orange background
+                my_pixel_put(x, y, &fractal->img, 0x004D4D);           // Dark teal background
             else
                 handle_pixel(x, y, fractal);
             x++;
