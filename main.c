97fefeb7135cb6b9f6b2fe15708a79632f965f35
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2025-05-05 10:50:24 +0800

    added some prints to test

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(int nargs, char **args)
 
     printf("nargs: %d\n", nargs);
     printf("args[1]: %s\n", args[1]);
-    printf("ft_strcmp result: %d\n", ft_strcmp(args[1], "julia", 10));
+    printf("ft_strcmp result: %d\n", !ft_strcmp(args[1], "julia", 10));
 
-    if ((nargs == 2 && ft_strcmp(args[1], "mandelbrot", 10) == 0) || (nargs == 4 && ft_strcmp(args[1], "julia", 10) == 0))
+    if ((nargs == 2 && !ft_strcmp(args[1], "mandelbrot", 10)) || (nargs == 4 && !ft_strcmp(args[1], "julia", 10)))
     {
         fractal.name = args[1];
         fractal.julia_x = alphtodoubl(args[2]);
