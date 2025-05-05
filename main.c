0fc53f205ecfb54e7d93148852994706aee8afb0
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2025-05-05 10:40:21 +0800

    fix:fix julia imput

--- main.c
+++ main.c
@@ -4,7 +4,16 @@ int main(int nargs, char **args)
 {
     t_fractal fractal;
 
-    if ((nargs == 2 && !ft_strcmp(args[1], "mandelbrot", 10)) || (nargs == 4 && !ft_strcmp(args[1], "julia", 10)))
+    if (nargs == 2 && !ft_strcmp(args[1], "mandelbrot", 10))
+    {
+        fractal.name = args[1];
+        fractal.julia_x = 0;
+        fractal.julia_y = 0;
+        fractal_init(&fractal);
+        fractal_render(&fractal);
+        mlx_loop(fractal.mlx_connection);
+    }
+    else if (nargs == 4 && !ft_strcmp(args[1], "julia", 10))
     {
         fractal.name = args[1];
         fractal.julia_x = alphtodoubl(args[2]);
